34d68cf722810a97385a9fcc834066b0a444a58f
priy23anka <105520837+priy23anka@users.noreply.github.com>
2022-07-20 00:19:11 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.getElementById('nav-btn').addEventListener('click', ()=>{
+    document.getElementById('nav-links').classList.toggle('show-links');
+})
+document.getElementById('date').textContent = new Date().getFullYear();
